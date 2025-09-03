58c025041ac15b48bfbd2ad5f06c0f2c71fc9fcd
CubeClubovich <SergoDJKEKC@gmail.com>
2025-09-03 23:20:15 +0300

    Created "Calculator" for Task 4.1 and "Days of the Week Chooser".

---
+++ Calculator_4.1/Calculator_4.1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+double num1, num2, result = 0;
+char op;
+bool valid = true;
+
+Console.WriteLine("Enter first number:");
+if (!double.TryParse(Console.ReadLine(), out num1))
+{
+    Console.WriteLine("Invalid number input!");
+    return;
+}
+
+Console.WriteLine("Enter operator (+, -, *, /):");
+if (!char.TryParse(Console.ReadLine(), out op))
+{
+    Console.WriteLine("Invalid operator input!");
+    return;
+}
+
+Console.WriteLine("Enter second number:");
+if (!double.TryParse(Console.ReadLine(), out num2))
+{
+    Console.WriteLine("Invalid number input!");
+    return;
+}
+
+switch (op)
+{
+    case '+': result = num1 + num2; break;
+    case '-': result = num1 - num2; break;
+    case '*': result = num1 * num2; break;
+    case '/':
+        if (num2 != 0) result = num1 / num2;
+        else { Console.WriteLine("Error: Cannot be divided by zero."); valid = false; }
+        break;
+    default:
+        Console.WriteLine("Invalid operator.");
+        valid = false;
+        break;
+}
+
+if (valid)
+    Console.WriteLine($"{num1} {op} {num2} = {result}");
---
+++ DaysOfTheWeekChooser_4.2/DaysOfTheWeekChooser_4.2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Choose the number of the day: ");
+string dow = Console.ReadLine();
+bool valid = true;
+
+switch (dow)
+{
+    case "1": dow = "Monday"; break;
+    case "2": dow = "Tuesday"; break;
+    case "3": dow = "Wednesday"; break;
+    case "4": dow = "Thursday"; break;
+    case "5": dow = "Friday"; break;
+    case "6": dow = "Saturday"; break;
+    case "7": dow = "Sunday"; break;
+    default: Console.WriteLine("Wrong symbols! Try to pick from 1 to 7!"); valid = false; break;
+}
+
+if (valid)
+    Console.WriteLine($"It's {dow}!");
