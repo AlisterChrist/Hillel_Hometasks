8526c1736f68f364c4a1442c501f13c431ee4224
CubeClubovich <SergoDJKEKC@gmail.com>
2025-09-04 11:37:38 +0300

    Created tester of the age.

---
+++ Age of the Person/Age of the Person/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.Design;
+
+Console.Write("Enter your age:");
+int age;
+if (!int.TryParse(Console.ReadLine(), out age))
+{
+    Console.WriteLine("Invalid number input!");
+    return;
+}
+if (age >= 18)
+{
+    Console.Write("Welcome again, my friend!");
+}
+else
+{
+    Console.Write("Sorry, you're not old enough for this.");
+}
