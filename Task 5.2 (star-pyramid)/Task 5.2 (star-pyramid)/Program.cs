4e8e3cf02ed4bc52ec154accd12a10b6b904ff38
CubeClubovich <SergoDJKEKC@gmail.com>
2025-09-07 11:38:56 +0300

    Created code for building a pyramid.

---
+++ Task 5.2 (star-pyramid)/Task 5.2 (star-pyramid)/Program.cs
@@ -0,0 +1,21 @@
+﻿
+int numRows;
+
+Console.WriteLine("How many stars will we build a pyramid from?");
+
+if (!int.TryParse(Console.ReadLine(), out numRows))
+{
+    Console.WriteLine("Invalid number input!");
+    return;
+}
+
+//One-sided Pyramid
+
+for (int i = 1; i <= numRows; i++)
+{
+    for (int j = 0; j < i; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
