1ba5266163cc6b063a5d3c68ddd459b15dbf88e9
CubeClubovich <SergoDJKEKC@gmail.com>
2025-09-07 12:47:09 +0300

    Created user-friendly Multiplication table code. You can multiply not only integers, but also decimals.

---
+++ Task 5.7 (Multiplication table)/Task 5.7 (Multiplication table)/Program.cs
@@ -0,0 +1,16 @@
+﻿
+double number;
+
+Console.Write("Enter number: ");
+if (!double.TryParse(Console.ReadLine(), out number))
+{
+    Console.WriteLine("Invalid number input!");
+    return;
+}
+
+for (int fromOnetoTen = 1; fromOnetoTen <= 10; fromOnetoTen++)
+{
+    double result = number * fromOnetoTen;
+
+    Console.WriteLine($"{number} * {fromOnetoTen} = {result}");
+}
