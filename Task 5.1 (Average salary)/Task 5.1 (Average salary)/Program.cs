b26621c866a0551fcaf5127fd1c4917ab4d07139
CubeClubovich <SergoDJKEKC@gmail.com>
2025-09-08 12:26:02 +0300

    Added salary calculator for the company.

---
+++ Task 5.1 (Average salary)/Task 5.1 (Average salary)/Program.cs
@@ -0,0 +1,25 @@
+﻿
+int employeeNumber;
+double totalSalary = 0;
+
+Console.WriteLine("How many employees on your workplace?");
+
+while (!int.TryParse(Console.ReadLine(), out employeeNumber))
+{
+    Console.WriteLine("Incorrect symbols!");
+}
+
+for (int workersNum = 0; workersNum < employeeNumber; workersNum++)
+{
+    Console.WriteLine($"Enter the salary of the worker #{workersNum+1}");
+
+    double salary;
+    while (!double.TryParse(Console.ReadLine(), out salary))
+    {
+        Console.WriteLine("Incorrect symbols!");
+    }
+
+    totalSalary += salary;
+}
+
+Console.WriteLine($"Average salary in the company: {totalSalary / employeeNumber}");
