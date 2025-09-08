f7bef82212066b11a47f58922bd36db15d898e99
CubeClubovich <SergoDJKEKC@gmail.com>
2025-09-08 12:59:12 +0300

    Created Fibanacci counting machine.

---
+++ Task 5.5 (Fibanacci counting)/Task 5.5 (Fibanacci counting)/Program.cs
@@ -0,0 +1,18 @@
+﻿int number;
+int current = 0;
+int previous = 0;
+
+Console.WriteLine("Enter the Number:");
+while (!int.TryParse(Console.ReadLine(), out number))
+{
+    Console.WriteLine("Incorrect symbols!");
+}
+
+for (var i = 0; i < number; i++)
+{
+    if (current == 0) current = i;
+    int buf = current;
+    current += previous;
+    previous = buf;
+    Console.WriteLine(current);
+}
