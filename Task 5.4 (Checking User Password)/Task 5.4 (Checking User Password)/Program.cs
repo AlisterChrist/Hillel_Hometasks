a2a5e522ed13be8cd4e4b8d9048fd6206d128047
CubeClubovich <SergoDJKEKC@gmail.com>
2025-09-09 11:57:48 +0300

    Created salary calculator for the company.

---
+++ Task 5.4 (Checking User Password)/Task 5.4 (Checking User Password)/Program.cs
@@ -0,0 +1,25 @@
+﻿
+double workHours;
+double salaryPerDay = 0;
+
+Console.WriteLine("How many hours do you work a day?");
+
+while (!double.TryParse(Console.ReadLine(), out workHours))
+{
+    Console.WriteLine("Incorrect symbols!");
+}
+
+for (int i = 0; i < workHours; i++)
+{
+    Console.WriteLine($"How much money do you earn for the {i + 1} hour?");
+
+    double salaryPerHour;
+    while (!double.TryParse(Console.ReadLine(), out salaryPerHour))
+    {
+        Console.WriteLine("Incorrect symbols!");
+    }
+
+    salaryPerDay += salaryPerHour;
+}
+
+Console.WriteLine($"Your salary in the company: {salaryPerDay}$");
