a4916eaba4fe83b50debf81307e9d3098c825b37
CubeClubovich <SergoDJKEKC@gmail.com>
2025-09-16 23:25:21 +0300

    Rename 7.1 Task where little tasks 1,2,3,4. And made little task 5.

---
+++ Task 7.1 (task 5)/Task 7.1 (task 5)/Program.cs
@@ -0,0 +1,27 @@
+﻿// Enum для днів тижня
+
+Console.Write("Enter the number of the days: ");
+
+if (int.TryParse(Console.ReadLine(), out int days))
+{
+// Обчислення індексу дня тижня
+int dayIndex = days % 7; // залишок від ділення на 7
+Days resultDay = (Days)dayIndex;
+
+Console.WriteLine($"In {days} days will be: {resultDay}");
+}
+else
+{
+Console.WriteLine("Wrong input!");
+}
+
+enum Days
+{
+    Monday = 0,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+}
