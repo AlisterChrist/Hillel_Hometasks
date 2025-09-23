a7e0f56379944eec91f26dde1da13b8a6695480d
CubeClubovich <SergoDJKEKC@gmail.com>
2025-09-23 11:14:42 +0300

    Created mahcine that removes all spaces in strings.

---
+++ Task 9.3 (Deleting spaces)/Task 9.3 (Deleting spaces)/Program.cs
@@ -0,0 +1,11 @@
+﻿// Вхідний рядок зі словами
+string input = "Laptop, Smartphone, Tablet, Headphones, Monitor, Keyboard, Mouse, Printer";
+
+Console.WriteLine("Origin string:");
+Console.WriteLine(input);
+
+// Видаляємо всі пробіли
+string result = input.Replace(" ", "");
+
+Console.WriteLine("\nString without spaces:");
+Console.WriteLine(result);
