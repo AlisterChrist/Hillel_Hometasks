3ed6ecf981d7879a96256c901572b5ad9ddfb10d
CubeClubovich <SergoDJKEKC@gmail.com>
2025-09-23 11:07:00 +0300

    Created check-machine for the first letter of First and Last names.

---
+++ Task 9.1 (Check First and Last name)/Task 9.1 (Check First and Last name)/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Enter your first and last name: ");
+string fullName = Console.ReadLine();
+
+// Розбиваємо рядок на ім’я та прізвище
+string[] parts = fullName.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+if (parts.Length < 2)
+{
+    Console.WriteLine("Please enter both your first and last name!");
+    return;
+}
+
+string firstName = parts[0];
+string lastName = parts[1];
+
+// Перевіряємо перші літери (регістр неважливий)
+if (char.ToUpper(firstName[0]) == char.ToUpper(lastName[0]))
+{
+    Console.WriteLine("Last name begins with the same letter as first name.");
+}
+else
+{
+    Console.WriteLine("The last name does not start with the same letter as the first name.");
+}
